86974c3dcfcb7bb7d339c48b2eb5258d4619fef4
Savenok Marta <magnolia.sav@gmail.com>
2023-09-27 23:59:30 +0300

    end

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -16,3 +16,45 @@ const swiper = new Swiper('.hotel-swiper', {
           }
       }
   });
+
+
+const burger = document.querySelector('.burger');
+const mobileMenu = document.querySelector('.mobile-menu');
+const closeBurger = document.querySelector('.menu-close-icon');
+const body = document.querySelector('body')
+
+function activateBurgerMenu() {
+    burger.addEventListener('click' , ()=> {
+        burger.classList.add('active');
+        mobileMenu.classList.add('active');
+        setTimeout(burger.classList.remove('active') , 1000);
+        body.style.overflow="hidden"
+    })
+}
+function closeBurgerMenu() {
+    closeBurger.addEventListener('click' , ()=> {
+        mobileMenu.classList.remove('active');
+        body.style.overflow="auto"
+    })
+}
+activateBurgerMenu();
+closeBurgerMenu() ;
+
+const mobileDropdownLinks = document.querySelectorAll('.mobile-dropdown-item');
+
+function activateMobileDropdown() {
+    mobileDropdownLinks.forEach(el => {
+        el.addEventListener('click' , (e)=> {
+            el.classList.toggle('active');
+            el.nextElementSibling.classList.toggle('active');
+        })
+    })
+}
+activateMobileDropdown();
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
